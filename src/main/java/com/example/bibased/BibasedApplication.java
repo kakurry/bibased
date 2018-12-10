bc8c610e94e516140059e085782ee582f7d8beb2
hedongxu <1641165306@qq.com>
2018-12-10 11:23:49 +0800

    MongoDB的使用类

--- src/main/java/com/example/bibased/BibasedApplication.java
+++ src/main/java/com/example/bibased/BibasedApplication.java
@@ -3,8 +3,9 @@ package com.example.bibased;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
-
+@EnableCaching
 @SpringBootApplication
 @MapperScan(value = "com.example.bibased.dao")
 public class BibasedApplication {
